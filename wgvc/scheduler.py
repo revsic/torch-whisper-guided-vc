ceb1fb3b0b321d8a2cf4cfe450716e88e801e2f7
revsic <revsic99@gmail.com>
2023-03-01 12:06:15 +0000

    scheduler: Add cosine scheduler

--- wgvc/scheduler.py
+++ wgvc/scheduler.py
@@ -1,25 +1,25 @@
 from typing import Tuple
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class Scheduler(nn.Module):
-    """Fixed noise scheduler, reference from DiffWave, Kong et al., 2021.
+    """Fixed noise scheduler, reference from Improved DDPM, Nichol & Dhariwal, 2021.
     """
-    def __init__(self, steps: int, start: float, end: float):
+    def __init__(self, steps: int, s: float = 0.008):
         """Initializer.
         Args:
             steps: the number of the diffusion steps.
-            start: beta start.
-            end: beta end.
+            s: offset parameter for preventing too small beta.
         """
         super().__init__()
-        self.register_buffer(
-            'beta',
-            torch.linspace(start, end, steps, dtype=torch.float32),
-            persistent=False)
+        # [steps + 1]
+        a = torch.arange(1 + steps) / steps
+        f = ((a + s) / (1 + s) * np.pi * 0.5).cos().square()
+        self.register_buffer('alpha', f / f[0], persistent=False)
 
     def forward(self) -> Tuple[torch.Tensor, Tuple]:
         """Compute beta values.
@@ -27,8 +27,10 @@ class Scheduler(nn.Module):
             [torch.float32; [1 + steps]], log-SNR.
             [torch.float32; [1 + steps]], beta values.
         """
+        # alias
+        a = self.alpha
         # [1 + steps]
-        beta = F.pad(self.beta, [1, 0])
-        # alpha_bar
-        logsnr = torch.cumprod(1 - beta, dim=-1).logit()
+        logsnr = a.logit()
+        # [1 + steps]
+        beta = F.pad(1 - a[1:] / a[:-1], [1, 0])
         return logsnr, beta
